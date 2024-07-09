1e7818708d987cb2eacf654c6aeab0847d483b6b
loremersol <163796297+loremersol@users.noreply.github.com>
2024-07-09 15:28:07 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDROk5TfX8rMu-HWwfASr0z2fYHuYZR8ac",
+  authDomain: "crud-sol.firebaseapp.com",
+  databaseURL: "https://crud-sol-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "crud-sol",
+  storageBucket: "crud-sol.appspot.com",
+  messagingSenderId: "527553965333",
+  appId: "1:527553965333:web:3a4e81d2c805353f984d1c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
